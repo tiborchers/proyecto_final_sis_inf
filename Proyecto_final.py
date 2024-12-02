73e370ff05a03ad9b6fea1548b5dca1eb7628ebc
Tomas Borchers <tiborchers@uc.cl>
2024-12-02 20:20:39 +0100

    ADD.: change input fields

--- Proyecto_final.py
+++ Proyecto_final.py
@@ -4,10 +4,10 @@ from influxdb_client import InfluxDBClient, Point, WriteOptions
 app = Flask(__name__)
 
 # Configuración de InfluxDB
-INFLUXDB_URL = "http://localhost:8086"  # URL de tu servidor InfluxDB
-INFLUXDB_TOKEN = "tu_token_aqui"  # Token de autenticación
-INFLUXDB_ORG = "tu_organizacion_aqui"  # Nombre de la organización
-INFLUXDB_BUCKET = "sensores"  # Nombre del bucket en InfluxDB
+INFLUXDB_URL = "https://eu-central-1-1.aws.cloud2.influxdata.com"  # URL de tu servidor InfluxDB
+INFLUXDB_TOKEN = ""  # Token de autenticación
+INFLUXDB_ORG = "ProyectoFinalSisInf"  # Nombre de la organización
+INFLUXDB_BUCKET = "proyecto_final"  # Nombre del bucket en InfluxDB
 
 # Crear cliente de InfluxDB
 client = InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
@@ -30,17 +30,18 @@ def read_sensors():
         content = request.get_json()
 
         # Validación de los campos
-        required_fields = ['temperatura', 'temperaturaMax', 'temperaturaMin', 'pos']
+        required_fields = ['temperatura', 'humedad', 'distancia1', 'distancia2', 'contador']
+        
         if not all(field in content for field in required_fields):
             return jsonify({"error": "Faltan campos en los datos recibidos"}), 400
 
         # Crear un punto para InfluxDB
         point = Point("mediciones") \
-            .tag("sensor", "temperatura") \
             .field("temperatura", content['temperatura']) \
-            .field("temperaturaMax", content['temperaturaMax']) \
-            .field("temperaturaMin", content['temperaturaMin']) \
-            .field("pos", content['pos'])
+            .field("humedad", content['humedad']) \
+            .field("distancia1", content['distancia1']) \
+            .field("distancia2", content["distancia2"]) \
+            .field("contador", content['contador'])
 
         # Escribir el punto en InfluxDB
         write_api.write(bucket=INFLUXDB_BUCKET, record=point)
@@ -49,9 +50,10 @@ def read_sensors():
         return jsonify({
             "status": "Recibido correctamente!",
             "temperatura": content['temperatura'],
-            "temperaturaMax": content['temperaturaMax'],
-            "temperaturaMin": content['temperaturaMin'],
-            "pos": content['pos']
+            "humedad": content['humedad'],
+            "distancia1": content['distancia1'],
+            "distancia2": content['distancia2'],
+            "contador": content['contador']
         }), 200
 
     except Exception as e:
@@ -74,7 +76,6 @@ def get_sensor_values():
             for record in table.records:
                 results.append({
                     "time": record.get_time(),
-                    "sensor": record["sensor"],
                     "field": record.get_field(),
                     "value": record.get_value()
                 })
